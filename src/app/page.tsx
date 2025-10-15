f77d111dcf4940ed3f0350ee450bfcbafb761022
chandrakumarreddy <chandrapenugonda655@gmail.com>
2025-10-15 11:13:03 +0530

    setup shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="font-sans min-h-screen min-w-screen flex items-center justify-center">
+      <Button variant="outline">Hello</Button>
+    </div>
+  );
+}
