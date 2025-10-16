63499d011b62d38ab6b7dfb54bc2ca572bafe5c7
chandrakumarreddy <chandrapenugonda655@gmail.com>
2025-10-16 20:14:13 +0530

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,12 @@ import { caller, getQueryClient, trpc } from "@/trpc/server";
 import { Suspense } from "react";
 
 export default async function Home() {
-  // const { data: users } = useQuery(trpc.getUsers.queryOptions());
-
-  // const users = await caller.getUsers();
-
   const queryClient = getQueryClient();
 
   void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
 
   return (
     <div className="font-sans min-h-screen min-w-screen flex items-center justify-center">
-      {/* <Client users={users} /> */}
       <HydrationBoundary state={dehydrate(queryClient)}>
         <Suspense fallback={<div>Loading...</div>}>
           <Client />
